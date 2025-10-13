8b4b08de4ba50ebf75e7f533acdb35a70d226989
Lama13372002 <130605808+Lama13372002@users.noreply.github.com>
2025-10-13 16:20:56 +0700

    Update models.go

--- backend/internal/models/models.go
+++ backend/internal/models/models.go
@@ -190,7 +190,7 @@ type CreatePlanRequest struct {
 }
 
 type UpdatePlanRequest struct {
-	ID          int      `json:"id" binding:"required"`
+	PlanID      int      `json:"plan_id" binding:"required"`
 	Name        string   `json:"name" binding:"required"`
 	Description *string  `json:"description"`
 	Price       float64  `json:"price" binding:"required"`
